10d286375900b9c4c8e6e74b614c339f31bdbf84
ike-kazu <ike.forwork@gmail.com>
2025-01-20 15:43:21 +0900

    fix gpsd bash

--- bash/whole.bash
+++ bash/whole.bash
@@ -1,13 +1,10 @@
 # gpsd
 mv /dev/ttyUL0 /dev/tty16
+killall -9 gpsd
 systemctl stop gpsd.socket gpsd.service
 sleep 1
 systemctl disable gpsd.socket gpsd.service
 sleep 1
-systemctl stop gpsd.service
-sleep 3
-systemctl start gpsd.service
-killall -9 gpsd
 gpsd -n -N /dev/tty16 &
 sleep 2
 echo "gpsd service started"
